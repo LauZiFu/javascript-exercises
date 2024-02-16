b26f93da9a59fc43a8e499c5a04bef22b4ef7f7e
LauZiFu <lau.zifu@gmail.com>
2024-02-16 09:36:55 +0800

    Complete 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,6 @@
-const reverseString = function() {
-
+const reverseString = function(input) {
+    let charArr = input.split("");
+    return charArr.reverse().join("");
 };
 
 // Do not edit below this line
