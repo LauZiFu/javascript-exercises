9957787657fedf8841cc4836578277234ec5f7be
LauZiFu <lau.zifu@gmail.com>
2024-02-16 13:42:59 +0800

    Complete 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,26 @@
-const fibonacci = function() {
 
+
+const fibonacci = function(num) {
+    let i = 2;
+    let firstPrev = 1;
+    let secondPrev = 1;
+    let res = 1;
+    num = +num;
+
+    if(num === 0){
+        return 0;
+    } else if (num < 0){
+        return "OOPS";
+    }
+
+    while(i < num){
+        res = firstPrev + secondPrev;
+        firstPrev = secondPrev;
+        secondPrev = res;
+        i++;
+    }
+
+    return res;
 };
 
 // Do not edit below this line
