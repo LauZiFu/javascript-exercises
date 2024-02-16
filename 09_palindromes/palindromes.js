432659eb5fb8b7b323dc030c5b87b6f783d7d573
LauZiFu <lau.zifu@gmail.com>
2024-02-16 12:14:07 +0800

    Complete 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
+const palindromes = function (sentence) {
+    //removes punctuation and spaces
+    sentence = sentence.replace(/[^\w\s]/g, "").replace(/\s+/g, "").toLowerCase();
 
+
+    let low = 0;
+    let high = sentence.length -1;
+
+    while(low < high){
+        if(sentence[low] !== sentence[high])
+            return false;
+        low++;
+        high--;
+    }
+
+    return true;
 };
 
 // Do not edit below this line
