6fcdb57f58cc820b9320e9987f9e636592a5479b
LauZiFu <lau.zifu@gmail.com>
2024-02-16 09:23:35 +0800

    Complete 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
+const repeatString = function(input, repetition) {
+    let result = "";
 
+    for(let i =0; i<repetition; i++){
+        result += input;
+    }
+
+    return result;
 };
 
 // Do not edit below this line
