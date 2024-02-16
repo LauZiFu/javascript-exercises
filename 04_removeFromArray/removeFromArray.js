7ffd555a38cbebfe2ff0af0b5d1dba90e0c27fcc
LauZiFu <lau.zifu@gmail.com>
2024-02-16 10:04:18 +0800

    Complete 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
 
+
+
+const removeFromArray = function(arr, ...args) {
+    let result = arr;
+
+    for(const arg of args){
+        while(result.indexOf(arg) >= 0){
+            result.splice(result.indexOf(arg), 1);
+        }
+    }
+    return result;
 };
 
 // Do not edit below this line
