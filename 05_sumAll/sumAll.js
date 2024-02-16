7acc946861711ae1ca18767087fb791595cde24a
LauZiFu <lau.zifu@gmail.com>
2024-02-16 10:32:21 +0800

    Complete 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,22 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+
+    if(!Number.isInteger(min) || !Number.isInteger(max) || min < 0 || max < 0){
+        return "ERROR";
+    } else if(min > max){
+        const temp = min;
+        min = max;
+        max = temp;
+    }
+
+    const calcSum = function(num){
+        return (num * (num + 1))/2;
+    }
+
+    return calcSum(max) - calcSum(min - 1);
+
 
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+
