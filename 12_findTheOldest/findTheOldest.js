509af8fb3fd1feefa55b25924523c50e50fc7ac4
LauZiFu <lau.zifu@gmail.com>
2024-02-19 11:47:52 +0800

    Complete 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
 
+
+const findTheOldest = function(arr) {
+    return arr.reduce((acc, obj) => {
+        let deathYear1 = acc.yearOfDeath;
+        let deathYear2 = obj.yearOfDeath;
+
+        if(deathYear1 === undefined)
+            deathYear1 = new Date().getFullYear();
+        if(deathYear2 === undefined)
+            deathYear2 = new Date().getFullYear();
+        return (deathYear1 - acc.yearOfBirth) > (deathYear2- obj.yearOfBirth) ? acc : obj;
+    }, {});
 };
 
 // Do not edit below this line
